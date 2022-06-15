e2707f9b8935af7d048ea91ce52f2f7dfc64b0b6
Beryl B <beriebaldwin@gmail.com>
2022-06-15 18:05:19 -0400

    i forget to commit

---
+++ data/skill-data.js
@@ -0,0 +1,11 @@
+const skills = [
+  {student: 'Jordin', skill: 'poptart flavor creation', level: 'high'},
+  {student: 'Scott', skill: 'emu herding', level: 'high'},
+  {student: 'Beryl', skill: 'being a marshmallow', level: 'high'},
+  {student: 'Past David', skill: 'levitation', level: 'not high'},
+  {student: 'Jake', skill: 'eating pringles', level: 'high'}
+]
+
+export {
+  skills
+}
---
+++ routes/skills.js
@@ -0,0 +1,16 @@
+import { Router } from 'express'
+import * as skillsCtrl from '../controllers/skills.js'
+
+const router = Router()
+
+/* GET users listing. */
+router.get('/', skillsCtrl.index)
+
+router.get('/new', skillsCtrl.new)
+
+
+router.post('/', skillsCtrl.create)
+
+export {
+  router
+}
